0eca473561e92d4207182988c8bf848f4bf2c410
Syed Hasan Samin <hasdocexe@gmail.com>
2020-12-08 20:21:42 +0000

    minor progress on problem - ommitted wrong code

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -24,18 +24,58 @@ def solve_5bd6f4ac(x):
     x = x[0:num_rows//3, 2*(num_cols//3):num_cols]
     return x
 
-# def solve_1a07d186(x):
-#     # step 1: find horizontal or verticle lines that contain none black continous lines. 
-#     print("####################")
-#     rows = x[0:, ]
-#     where = np.(x[0:,] != 0)
-#     print("something---->\n", where)
-#     # if np.any(rows != 0):
-#     #     print("something")
-#     #     row.
-#     # thing = np.all(rows != 0)
-#     # print(thing)
-#     return x
+def solve_1a07d186(x):
+#   step 1: find horizontal or verticle lines that contain none black continous lines.
+    # If the lines are horizontal, the followuing will return index of rows in x 
+    num_rows = x.shape[0]
+    num_cols = x.shape[1]
+
+    def solver(x):
+        # get the coords for all non-zero entries
+        non_z = np.where(x != 0)
+        row = non_z[0]
+        col = non_z[1]
+        nz_cords = np.dstack((row,col))
+        
+        line_ind = list()
+        line_vals = list()
+        # get the index and val arrays for where the lines are
+        # index here contains the row co-ord for the grid
+        for index, vals in enumerate(x):
+            if np.all(vals != 0):
+                line_vals.append(vals)
+                line_ind.append(index)
+        
+        # get one of the values from each of the lines for comparison
+        one_val_grid = [elems[0] for elems in line_vals]
+
+
+        points = list()
+        points_idx = list()
+        # where the individual occurences of the random points are
+        for idx, vals in enumerate(x):
+            if idx not in grid_ind:
+                if np.all(vals == 0): pass
+                else:
+                    points_idx.append(idx)
+                    points.append(vals)
+        
+        return x
+    # find if there are values occurring repeatedly in the vertical
+    col_vac_x = np.where(np.bitwise_or.reduce(x) != 0)
+
+    # if the returned values is the same length as the number of columns than the grids are 
+    # horizontal
+    if len(col_val_x[0]) == num_rows:
+        #  call internal solver func to perform rest of the necessary alg steps
+        solver(x)
+    else:
+        x = x.transpose()
+         #  call internal solver func to perform rest of the necessary alg steps
+         solver(x)
+         x = x.transpose()
+         return x
+    return x
 
 def main():
     # Find all the functions defined in this file whose names are
