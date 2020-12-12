1f3b0646fba4fed44b9b8726ab90f4e5660fbffd
Syed Hasan Samin <hasdocexe@gmail.com>
2020-12-12 01:17:16 +0000

    second problem functioning, removed useless code, todo: update docs for this function and finish wip for third problem and trasfer and test from notebook to code

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -15,66 +15,97 @@ import re
 ### Name: Hasan Syed
 ### URL: https://github.com/SHS10/ARC 
 
-def solve_5bd6f4ac(x):
-    # get shape data in different categories
-    num_rows = x.shape[0]
-    num_cols = x.shape[1]
-    # perform integer division to get the right quarter of the array 
-    # regardless of dimensions for extensibility to different dimensions.
-    x = x[0:num_rows//3, 2*(num_cols//3):num_cols]
-    return x
+# def solve_5bd6f4ac(x):
+#     # get shape data in different categories
+#     num_rows = x.shape[0]
+#     num_cols = x.shape[1]
+#     # perform integer division to get the right quarter of the array 
+#     # regardless of dimensions for extensibility to different dimensions.
+#     x = x[0:num_rows//3, 2*(num_cols//3):num_cols]
+#     return x
 
 def solve_1a07d186(x):
 #   step 1: find horizontal or verticle lines that contain none black continous lines.
     # If the lines are horizontal, the followuing will return index of rows in x 
-    num_rows = x.shape[0]
-    num_cols = x.shape[1]
 
+    contains_horizontal_boole_arr = [np.all(elems != 0) for elems in x]
+    horizontal = False
+    if any(contains_hz_boole == True for contains_hz_boole in contains_horizontal_boole_arr):
+        horizontal = True
+    
     def solver(x):
-        # get the coords for all non-zero entries
-        non_z = np.where(x != 0)
-        row = non_z[0]
-        col = non_z[1]
-        nz_cords = np.dstack((row,col))
+        cp_x = x.copy()
+        def get_non_zero_cords(x): 
+        # get the coords for non-zero values
+            non_z = np.where(x != 0)
+            row = non_z[0]
+            col = non_z[1]
+            nz_cords = np.dstack((row,col))
+            # remove extra unused dimension from the array
+            nz_cords = nz_cords[0, :, :]
+            return nz_cords
+
+        nz_cords = get_non_zero_cords(x)
         
-        line_ind = list()
+        # detect the lines  
         line_vals = list()
-        # get the index and val arrays for where the lines are
-        # index here contains the row co-ord for the grid
-        for index, vals in enumerate(x):
-            if np.all(vals != 0):
-                line_vals.append(vals)
-                line_ind.append(index)
+        line_idx_row = list()
+        for index, elems in enumerate(x):
+            if np.all(elems != 0):
+                line_vals.append(elems)
+                line_idx_row.append(index)
         
-        # get one of the values from each of the lines for comparison
-        one_val_grid = [elems[0] for elems in line_vals]
-
-
-        points = list()
-        points_idx = list()
-        # where the individual occurences of the random points are
-        for idx, vals in enumerate(x):
-            if idx not in grid_ind:
-                if np.all(vals == 0): pass
-                else:
-                    points_idx.append(idx)
-                    points.append(vals)
+        # use both nz_cords with along the detected lines to find the unique 
+        # squares that need to be accounted for
         
-        return x
+        line_val_single = [elems[0] for elems in line_vals]
+        
+        # replace grids with non-continous line values with black.
+        for cords in nz_cords:
+            if np.any(cp_x[cords[0],cords[1]] not in line_val_single):
+                cp_x[cords[0],cords[1]] = 0
+        
+        # refresh the non-zero cords since update above.
+        nz_cords = get_non_zero_cords(cp_x)
+        
+        outliers_idx = list()   
+        for cords in nz_cords:
+            if cords[0] not in line_idx_row:
+                    outliers_idx.append(cords)
+            
+        outliers_idx = np.array(outliers_idx)
+
+        for index, elems in enumerate(outliers_idx):
+        # get the ouliers vals - the colors themselves    
+        # compare to against values 
+            if cp_x[elems[0],elems[1]] in line_val_single:
+                outlier_val = cp_x[elems[0],elems[1]]
+                for idx in line_idx_row:
+                    val_at_line = cp_x[idx][0]
+                    if outlier_val == val_at_line:
+                        outlier_row = elems[0]
+                        if outlier_row < idx:
+                            cp_x[elems[0],elems[1]] = 0
+                            cp_x[idx-1, elems[1]] = outlier_val
+                        if outlier_row > idx:
+                            cp_x[elems[0],elems[1]] = 0
+                            cp_x[idx+1, elems[1]] = outlier_val
+            
+        return cp_x
+
     # find if there are values occurring repeatedly in the vertical
-    col_vac_x = np.where(np.bitwise_or.reduce(x) != 0)
-
     # if the returned values is the same length as the number of columns than the grids are 
     # horizontal
-    if len(col_val_x[0]) == num_rows:
-        #  call internal solver func to perform rest of the necessary alg steps
-        solver(x)
+    # x = solver(x)
+    if horizontal:
+        x = solver(x)
+        return x
     else:
         x = x.transpose()
-         #  call internal solver func to perform rest of the necessary alg steps
-         solver(x)
-         x = x.transpose()
-         return x
+        x = solver(x)
+        x = x.transpose()
+        return x
+
     return x
 
 def main():
